9276eaecaa6619219286f8cf0c34278175ea7473
NoelMacwan <yulespencer@gmail.com>
2015-11-11 15:57:33 +0530

    defconfig: fix touchscreen problem for TWRP
    cherry-picked from https://github.com/NoelMacwan/SXDNickiMallow/commit/04276740b231aede5f3de8718eb1085bfb4c5e74

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
  #
- # Copyright © 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
+ # Copyright ï¿½ 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
  #
  # Custom build script
  #
@@ -16,27 +16,13 @@
 #!/bin/bash
 # export CROSS_COMPILE="/root/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-"
 # export CROSS_COMPILE="/root/cm11/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-"
-export CROSS_COMPILE="/root/linaro/4.9.3-2014.12.20141230.CR83/bin/arm-eabi-"
-STRIP="/root/linaro/4.9.3-2014.12.20141230.CR83/bin/arm-eabi-strip"
-MODULES_DIR="../modules"
-ZIMAGE="/root/tz/arch/arm/boot/zImage"
-KERNEL_DIR="/root/tz"
-MKBOOTIMG="/root/tz/tools/mkbootimg"
-MKBOOTFS="/root/tz/tools/mkbootfs"
+export CROSS_COMPILE="/home/noelmacwan/kernel/linaro-4.9/bin/arm-eabi-"
+STRIP="/home/noelmacwan/kernel/linaro-4.9/bin/arm-eabi-strip"
+MODULES_DIR="/home/noelmacwan/kernel/mm/modules"
+KERNEL_DIR="/home/noelmacwan/kernel/mm"
 BUILD_START=$(date +"%s")
 export ARCH=arm
 export SUBARCH=arm
-export KBUILD_BUILD_USER="varun.chitre15"
-export KBUILD_BUILD_HOST="EroticBox"
-if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
-then
-rm $ZIMAGE
-rm $MODULES_DIR/*
-fi
-make cyanogenmod_taoshan_defconfig
-make -j32
-if [ -a $ZIMAGE ];
-then
 echo "Copying modules"
 rm $MODULES_DIR/*
 find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
@@ -45,13 +31,3 @@ echo "Stripping modules for size"
 $STRIP --strip-unneeded *.ko
 zip -9 modules *
 cd $KERNEL_DIR
-echo "Creating boot image"
-$MKBOOTFS ramdisk/ > $KERNEL_DIR/ramdisk.cpio
-cat $KERNEL_DIR/ramdisk.cpio | gzip > $KERNEL_DIR/root.fs
-$MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2" --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 -o $KERNEL_DIR/boot.img
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-else
-echo "Compilation failed! Fix the errors!"
-fi
